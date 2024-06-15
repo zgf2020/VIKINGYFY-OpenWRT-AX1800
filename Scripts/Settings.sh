6aedad8c741a19646cf3734884765968835530a4
zgf2020 <103357272+zgf2020@users.noreply.github.com>
2024-06-15 10:22:19 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -33,7 +33,7 @@ echo "CONFIG_PACKAGE_luci-theme-$WRT_THEME=y" >> ./.config
 echo "CONFIG_PACKAGE_luci-app-$WRT_THEME-config=y" >> ./.config
 
 if [[ $WRT_URL == *"lede"* ]]; then
-	echo "CONFIG_PACKAGE_luci-app-ssr-plus=y" >> ./.config
+	echo "CONFIG_PACKAGE_luci-app-ssr-plus=n" >> ./.config
 	echo "CONFIG_PACKAGE_luci-app-openclash=y" >> ./.config
 else
 	echo "CONFIG_PACKAGE_luci=y" >> ./.config
